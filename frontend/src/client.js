30bc708b657ddc21fdad111d0a6b5440b416aad0
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-23 02:08:53 -0400

    Implement .env fix for deployment

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: 'mhpj9g44',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: 'skHBeb54u5m9zqb2P8EuhHBG7CcVuNLADqJFIewoIVTavT5ml8805B0TKGrKcT73zp3t4nPP3vXxTac1nO4wYR7ZgA47qaDbPke4YQYwfzSVkkiBJ3FNF13lEbScaRryxpyfRQTGZztHYsEkNRpo0Zf6H6nVmy3m5EX5jB1pv2TNJrWz9wse',
+  token: process.env.REACT_APP_SANITY_PROJECT_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
