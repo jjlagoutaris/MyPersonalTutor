c4b438883d07dcb3a604667eb13e2b3ac3581c9b
John Lagoutaris <jjlagoutaris@gmail.com>
2023-03-23 02:01:34 -0400

    Testing

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: 'mhpj9g44',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: 'skHBeb54u5m9zqb2P8EuhHBG7CcVuNLADqJFIewoIVTavT5ml8805B0TKGrKcT73zp3t4nPP3vXxTac1nO4wYR7ZgA47qaDbPke4YQYwfzSVkkiBJ3FNF13lEbScaRryxpyfRQTGZztHYsEkNRpo0Zf6H6nVmy3m5EX5jB1pv2TNJrWz9wse',
 });
 
 const builder = imageUrlBuilder(client);
